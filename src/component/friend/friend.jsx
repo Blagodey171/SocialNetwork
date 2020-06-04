00ce794b24312b8b9c8375427b31609132de6652
Blagodey171 <perelad797@gmail.com>
2020-06-04 23:33:48 +0700

    create friends link <a>

---
+++ src/component/friend/friend.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './friend.module.css'
+
+const friend = (props) => {
+    return (
+        <div className='container'>
+            <a href="#">Friends</a>
+        </div>
+    )
+}
+
+export default friend;
--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import style from './header.module.css'
+import style from './header.module.css';
+import Friends from '../friend/friend'
 
 const header = (props) => {
     return (
         <div className={style.container}>
             <header className={style.header}>
                 <div className={style.logo}>
-                    <h1>Header</h1>
+                    <h1>socialTrain</h1>
+                    <Friends />
                 </div>
                 <div className={style.fastLink}>
 
