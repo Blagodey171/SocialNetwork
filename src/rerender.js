b7585744929a20c0fb1e1023058e6385c7313fe9
Blagodey171 <perelad797@gmail.com>
2020-07-21 01:15:34 +0700

    how to transfer addPost to profile without import to rerender?

--- src/rerender.js
+++ src/rerender.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
 import App from './component/app/App';
-import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import {addPost} from './state/state'
 
@@ -24,7 +22,4 @@ export let rerender = function (state) {
 
 
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
