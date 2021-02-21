c54b9d1827f21f55c46d962a3f321a47b0c8a1e8
Blagodey171 <perelad797@gmail.com>
2021-02-22 00:06:43 +0700

    app styles change, refactoring of the burger menu, events for closing the menu

--- src/component/fastLinks/burgerMenu.jsx
+++ src/component/fastLinks/burgerMenu.jsx
@@ -1,31 +1,48 @@
 import React, { useEffect } from 'react';
 import './burgerMenu.scss';
 
-const BurgerMenu = ( props ) => {
+const BurgerMenu = (props) => {
     useEffect(() => {
         const burgerMenu = document.querySelector('.burgerMenu-container');
         const sidebar = document.querySelector('.container-sidebar');
         const content = document.querySelector('.content-main');
         const overlayDiv = document.createElement('div');
-        overlayDiv.classList.add('overlay')
-        burgerMenu.addEventListener('click', (e) => {
+        overlayDiv.classList.add('overlay');
+
+        let removeListener = () => {
+            window.removeEventListener('resize', () => { })
+            window.removeEventListener('click', () => { })
+        }
+
+        window.addEventListener('click', (e) => {
             e.preventDefault();
-            burgerMenu.classList.toggle('burgerMenu-active');
-            if (burgerMenu.classList.contains('burgerMenu-active')) {
-                sidebar.classList.add('activeBurgerMenu');
-                content.insertAdjacentElement('beforebegin', overlayDiv)
+            console.log(e.target)
+            if (e.target === burgerMenu || e.target.parentNode === burgerMenu) {
+                burgerMenu.classList.toggle('burgerMenu-active');
+                if (burgerMenu.classList.contains('burgerMenu-active')) {
+                    sidebar.classList.add('activeBurgerMenu');
+                    content.insertAdjacentElement('beforebegin', overlayDiv)
+                } else {
+                    sidebar.classList.remove('activeBurgerMenu');
+                    overlayDiv.remove()
+                }
             } else {
+                if(e.target === sidebar)return
+                burgerMenu.classList.remove('burgerMenu-active');
                 sidebar.classList.remove('activeBurgerMenu');
                 overlayDiv.remove()
             }
         })
-
-        return burgerMenu.removeEventListener('click', ()=>{})
+        window.addEventListener('resize', e => {
+            if (burgerMenu.classList.contains('burgerMenu-active')) {
+                burgerMenu.classList.remove('burgerMenu-active');
+                sidebar.classList.remove('activeBurgerMenu');
+                overlayDiv.remove()
+            }
+        })
+        return removeListener();
     })
 
-    useEffect(() => {
-
-    })
     return (
         <div className='burgerMenu-container'>
             <div className='baseLine'></div>
